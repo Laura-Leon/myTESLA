31510f5350fe81dd79559ba1a16c45285449b0ef
laura León <isabelaleon8@gmail.com>
2020-03-11 08:00:59 -0500

    cambie rect

--- src/main/Carro.java
+++ src/main/Carro.java
@@ -16,7 +16,7 @@ public class Carro {
 		this.app = app;
 	}
 	public void comparaVel(){
-		app.rect(104,265,20,this.velocidad);
+		app.rect(104,265,this.velocidad,20);
 		this.velocidad -= 5;
 		if(this.velocidad <= -100) {
 			this.velocidad = -100;
